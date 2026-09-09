25421aaf8bd9e8333c74bd479ccd5ace269b09de
MBonandrini <mbonandrini82@gmail.com>
2026-09-09 12:20:51 +0100

    Delete apps directory

--- apps/schedule-builder/cleanup.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("pagehide", () => {
-    try {
-        if (typeof disposeAIEngines === "function") {
-            Promise.resolve(disposeAIEngines()).catch(() => {});
-        }
-    } catch (_) {}
-});
